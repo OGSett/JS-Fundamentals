d4dd78b7391bbd71bbbe8c7bfa1610de10183cf9
OGSett <ecomseth@gmail.com>
2025-05-29 19:47:33 +0100

    5-to_integer.js corrected

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ if (process.argv[2]) {
     if (isNaN(process.argv[2])) {
         console.log('is not a number')
     } else {
-        console.log('My number: ',parseInt(process.argv[2]))
+        console.log('My number:',parseInt(process.argv[2]))
     }
 } else {
     console.log('is not a number')
