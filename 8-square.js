9c4db2668c459088fefcee26291ea611fa98b645
OGSett <ecomseth@gmail.com>
2025-05-29 22:15:55 +0100

    8-square.js corrected 2

--- 8-square.js
+++ 8-square.js
@@ -1,12 +1,15 @@
-if(process.argv[2] && process.argv[3]) {
+if(process.argv[2] || process.argv[3]) {
     let x = parseInt(process.argv[2])
     let y = parseInt(process.argv[3])
 
-    if (isNaN(x) || isNaN(y)) {
-        console.log('Missing number of occurrences')
+    if (isNaN(x) && isNaN(y)) {
+        console.log('Missing size')
     } else {
         for (let i = 0; i < x; i++) {
             let test = ''
+            if (!y) {
+                y = x
+            } 
             for (let k = 0; k < y; k++) {
                 test += 'x'
             }
@@ -14,5 +17,5 @@ if(process.argv[2] && process.argv[3]) {
         } 
     }
 } else {
-    console.log('Missing number of occurrences')
+    console.log('Missing size')
 }
