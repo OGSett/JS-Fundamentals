38c3bfba26968e18151604344e61b432029380fb
OGSett <ecomseth@gmail.com>
2025-05-29 19:23:22 +0100

    correcting 2-args.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,5 +1,8 @@
-if(process.argv[2]) {
-console.log('Arguments found')
+if (process.argv.length === 2) {
+	console.log('No argument', )
+}
+else if(process.argv[2] && process.argv[3]) {
+	console.log('Arguments found')
 } else {
-	console.log('No argument')
+	console.log('Argument found')
 }
